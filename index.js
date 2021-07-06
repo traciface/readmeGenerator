a872b2adae7928dd20878f594956678a0254a54d
Traci <traci.face@gmail.com>
2021-07-06 13:46:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+
+
+// TODO: Create a function to write README file
+
+// TODO: Create a function to initialize app
+
+
+
+// TODO: Create a function to initialize app
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,4 @@
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+
+// TODO: Create a function to generate markdown for README
